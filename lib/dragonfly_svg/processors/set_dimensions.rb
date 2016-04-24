9284550689b062fbab19deae040da8bf6db3a8a2
Tomas Celizna <tomas.celizna@gmail.com>
2016-04-25 00:05:26 +0200

    no need to return content

--- lib/dragonfly_svg/processors/set_dimensions.rb
+++ lib/dragonfly_svg/processors/set_dimensions.rb
@@ -6,7 +6,6 @@ module DragonflySvg
       def call(content, width, height)
         SetAttribute.new.call(content, "//*[name()='svg']", 'width', width) if width
         SetAttribute.new.call(content, "//*[name()='svg']", 'height', height) if height
-        content
       end
     end
   end
