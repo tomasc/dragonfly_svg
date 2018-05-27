6782c3f38f5a5a4d1bcb1a4dcac0b437284bbfcb
Tomáš Celizna <tomas.celizna@gmail.com>
2018-05-27 22:46:28 +0200

    improve reporting

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,12 +2,15 @@ require 'bundler/setup'
 
 require 'minitest'
 require 'minitest/autorun'
+require 'minitest/reporters'
 require 'minitest/spec'
 
 require 'dragonfly'
 require 'dragonfly_svg'
 
-SAMPLES_DIR = Pathname.new(File.expand_path('../../samples', __FILE__))
+SAMPLES_DIR = Pathname.new(File.expand_path('../samples', __dir__))
+
+Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
 def test_app(name = nil)
   Dragonfly::App.instance(name).tap do |app|
